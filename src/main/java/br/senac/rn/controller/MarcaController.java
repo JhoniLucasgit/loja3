cc3904cfa12a983d732e7d647c71a5c74f30bfb2
JHONATANN LUCAS DE LIMA VASCONCELOS <a9244222@rn.senac.br>
2018-10-04 19:04:44 -0300

    fonts + icones no static 04/10/2018

--- src/main/java/br/senac/rn/controller/MarcaController.java
+++ src/main/java/br/senac/rn/controller/MarcaController.java
@@ -1,19 +1,29 @@
 package br.senac.rn.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.senac.rn.loja.model.Marca;
 import br.senac.rn.loja.service.MarcaService;
 
-@Controller("/")
+@Controller
+@RequestMapping("/marca")
 public class MarcaController {
 
 	@Autowired
 	private MarcaService service;
-	
-	@GetMapping("/listar")
-	private String listar() {
-		return null;
+		
+	@GetMapping("/lista")//requisição
+	public String listar(Model model) {
+		List<Marca> marcas = service.buscar();
+		model.addAttribute("marcas", marcas);
+		return "marca/lista";//pasta templates
 	}
+	
 }
--- src/main/java/br/senac/rn/loja/Loja3Application.java
+++ src/main/java/br/senac/rn/loja/Loja3Application.java
@@ -1,25 +1,13 @@
 package br.senac.rn.loja;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.senac.rn.loja.repository.MarcaRepository;
-
 @SpringBootApplication
-public class Loja3Application implements CommandLineRunner{
+public class Loja3Application {
 	
-	@Autowired //força uma instancia de qualquer coisa, seja interface, seja classe ou qualquer outro tipo
-	private MarcaRepository repository;
-
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Loja3Application.class);
-		app.run(args);
+		SpringApplication.run(Loja3Application.class);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(repository.buscaPorNome("Nike"));
-	}
 }
